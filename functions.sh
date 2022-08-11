76fe20fa4e86a287a8553f58d0ba1b4d9f152552
ajfazan <ajfazan@gmail.com>
2022-08-11 18:46:34 -0300

    Code refactoring.

--- functions.sh
+++ functions.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-fix_rgb_background() {
+create_alpha_band() {
+
+  RASTER=$(realpath ${1})
 
   DIR=$(realpath ${2})
 
-  cp ${1} ${DIR}
-
-  generate_raster_footprint --remove-holes --overwrite ${1} ${DIR}
+  generate_raster_footprint --skip-validation --remove-holes --overwrite --nodata 0 ${RASTER} ${DIR}
 
   LAYER=$(basename ${1} | sed -r 's/^(.+)\.(.+)$/\1/')
 
   FOOTPRINT="${DIR}/${LAYER}.gpkg"
-
-  TARGET="${DIR}/${LAYER}.tif"
 
   ogrinfo -q -sql "ALTER TABLE '${LAYER}' RENAME TO footprint" ${FOOTPRINT}
 
@@ -33,33 +31,47 @@ fix_rgb_background() {
   gdalinfo ${1} | egrep '^Size' | cut -d' ' -f3- | sed -r 's/,//g' > ${CSV2}
   read COLS ROWS < ${CSV2}
 
+  ALPHA="${DIR}/${LAYER}.alpha.tif"
+
+  gdal_rasterize -q -of GTiff -ot Byte -burn ${3} -l footprint \
+    -ts ${COLS} ${ROWS} -te ${XMIN} ${YMIN} ${XMAX} ${YMAX} ${FOOTPRINT} ${ALPHA}
+
+  rm -f ${FOOTPRINT} ${BBOX} ${CSV1} ${CSV2}
+
+  echo ${ALPHA}
+}
+
+fix_rgb_background() {
+
+  ALPHA=$(create_alpha_band ${1} ${2} 255)
+
+  DIR=$(dirname ${ALPHA})
+
+  TARGET="${DIR}/$(basename ${ALPHA} | sed -r 's/\.alpha\.tif$/.tif/')"
+
   R_BAND=$(mktemp -u -q --tmpdir=${TMPDIR} --suffix=".tif" XXXXXXXXXXXXXXXX)
   G_BAND=$(mktemp -u -q --tmpdir=${TMPDIR} --suffix=".tif" XXXXXXXXXXXXXXXX)
   B_BAND=$(mktemp -u -q --tmpdir=${TMPDIR} --suffix=".tif" XXXXXXXXXXXXXXXX)
-  A_BAND=$(mktemp -u -q --tmpdir=${TMPDIR} --suffix=".tif" XXXXXXXXXXXXXXXX)
 
-  gdal_rasterize -q -of GTiff -ot Byte -burn 255 -l footprint -ts ${COLS} ${ROWS} \
-    -te ${XMIN} ${YMIN} ${XMAX} ${YMAX} ${FOOTPRINT} ${A_BAND}
+  gdal_calc.py -A ${1} --A_band=1 -B ${ALPHA} --B_band=1 --type=Byte --format=GTiff --quiet \
+    --NoDataValue=0 --outfile=${R_BAND} --calc="( B != 0 ) * ( A + ( A == 0 ) )" &
 
-  gdal_edit.py -unsetnodata ${TARGET}
+  gdal_calc.py -A ${1} --A_band=2 -B ${ALPHA} --B_band=1 --type=Byte --format=GTiff --quiet \
+    --NoDataValue=0 --outfile=${G_BAND} --calc="( B != 0 ) * ( A + ( A == 0 ) )" &
 
-  gdal_calc.py -A ${TARGET} --A_band=1 -B ${A_BAND} --B_band=1 --type=Byte --format=GTiff --quiet \
-    --NoDataValue=0 --outfile=${R_BAND} --calc="( A * ( B != 0 ) ) + ( A == 0 ) * ( B != 0 )" &
-
-  gdal_calc.py -A ${TARGET} --A_band=2 -B ${A_BAND} --B_band=1 --type=Byte --format=GTiff --quiet \
-    --NoDataValue=0 --outfile=${G_BAND} --calc="( A * ( B != 0 ) ) + ( A == 0 ) * ( B != 0 )" &
-
-  gdal_calc.py -A ${TARGET} --A_band=3 -B ${A_BAND} --B_band=1 --type=Byte --format=GTiff --quiet \
-    --NoDataValue=0 --outfile=${B_BAND} --calc="( A * ( B != 0 ) ) + ( A == 0 ) * ( B != 0 )" &
+  gdal_calc.py -A ${1} --A_band=3 -B ${ALPHA} --B_band=1 --type=Byte --format=GTiff --quiet \
+    --NoDataValue=0 --outfile=${B_BAND} --calc="( B != 0 ) * ( A + ( A == 0 ) )" &
 
   wait
 
-  gdal_merge.py -q -o ${TARGET} -ot Byte -of GTiff -n 0 -a_nodata 0 -separate \
-    ${R_BAND} ${G_BAND} ${B_BAND}
+  gdal_merge.py -q -o ${TARGET} -ot Byte -of GTiff -co COMPRESS=LZW -co TILED=YES \
+    -n 0 -a_nodata 0 -separate ${R_BAND} ${G_BAND} ${B_BAND}
 
   gdal_edit.py -stats -colorinterp_1 red -colorinterp_2 green -colorinterp_3 blue ${TARGET}
 
-  rm -f ${R_BAND} ${G_BAND} ${B_BAND} ${A_BAND} ${BBOX} ${CSV1} ${CSV2}
+  rm -f ${R_BAND} ${G_BAND} ${B_BAND} ${ALPHA}
+
+  generate_raster_footprint --overwrite ${TARGET} ${DIR}
 
   return 0
 }
